38592216290cc2c850a13cb139a8cb6bb2c56861
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-06-21 14:28:16 +0200

    build: add spa example

---
+++ examples/spa/src/lib/VisitorData.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-svelte';
+
+	export let immediate = false;
+
+	const { getData, data, isLoading, error } = useVisitorData(
+		{ extendedResult: true },
+		{ immediate }
+	);
+</script>
+
+<div>
+	<button id="get_data" on:click={() => getData()}> Get visitor data </button>
+	{#if $isLoading}
+		<div id="loading">Loading...</div>
+	{/if}
+	{#if $error}
+		<div id="error">{$error.message}</div>
+	{/if}
+	{#if $data}
+		<pre id="data">
+      {JSON.stringify($data, null, 2)}
+    </pre>
+	{/if}
+</div>
