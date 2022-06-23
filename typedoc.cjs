89282c05c0367d2c3f262946349cdfbd7e50e124
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-06-23 12:46:55 +0200

    build: fix typedoc

--- typedoc.cjs
+++ typedoc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   excludePrivate: true,
   excludeProtected: true,
   tsconfig: './tsconfig.docs.json',
-  mode: "modules",
   out: {
     sort: ['source-order', 'static-first'],
   },
