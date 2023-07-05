ba8e237c28c9c2d7784d954d35aa75e6e33d325a
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-07-05 13:02:34 +0200

    feat: add reexports for `defaultEndpoint`, `defaultTlsEndpoint`, `defaultScriptUrlPattern`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,14 +2,19 @@ export type {
   CacheLocation,
   Cacheable,
   ICache,
-  LocalStorageCache,
-  SessionStorageCache,
-  InMemoryCache,
   LoadOptions,
   VisitorData,
   GetResult,
   ExtendedGetResult,
   GetOptions,
+} from '@fingerprintjs/fingerprintjs-pro-spa';
+export {
+  defaultEndpoint,
+  defaultTlsEndpoint,
+  defaultScriptUrlPattern,
+  LocalStorageCache,
+  SessionStorageCache,
+  InMemoryCache,
 } from '@fingerprintjs/fingerprintjs-pro-spa';
 
 export * from './types';
